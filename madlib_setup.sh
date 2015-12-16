35b03e82d46ce7cca718604a9a9bf13b4f76b80f
Gabriele Modena <gm@nowave.it>
2015-12-16 22:09:52 +0100

    madlib version bump
    Dockerfile and install have been updated to source madlib 1.8 and
    postgresql 9.4.

--- madlib_setup.sh
+++ madlib_setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-export HOST=127.0.0.1:5432
-export DATABASE=maddb
-export USER=madlib
-export DBMS=postgres
-export PGPASSWORD=password
+HOST=127.0.0.1:5432
+DATABASE=maddb
+USER=madlib
+DBMS=postgres
+PGPASSWORD=password
+
+PGVERSION=9.4
+
+su --login postgres --command "/usr/pgsql-${PGVERSION}/bin/postgres -D /var/lib/pgsql/${PGVERSION}/data -p 5432" &
+# ugly busy wait
+sleep 10
+ps aux 
 
 su --login - postgres --command "PGPASSWORD=${PGPASSWORD} /usr/local/madlib/bin/madpack -p $DBMS -c $USER@$HOST/$DATABASE install"
 
--- postgres_user.sh
+++ postgres_user.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+PGVERSION=9.4
 
-su --login postgres --command "/usr/pgsql-9.2/bin/postgres -D /var/lib/pgsql/9.2/data -p 5432" &
+su --login postgres --command "/usr/pgsql-${PGVERSION}/bin/postgres -D /var/lib/pgsql/${PGVERSION}/data -p 5432" &
 # ugly busy wait
 sleep 10
 ps aux 
 
-su --login - postgres --command "/usr/pgsql-9.2/bin/psql -c \"CREATE USER madlib with CREATEROLE superuser PASSWORD 'password';\""
-su --login - postgres --command "/usr/pgsql-9.2/bin/psql -c \"CREATE DATABASE maddb;\""
-su --login - postgres --command "/usr/pgsql-9.2/bin/psql -c \"\du;\""
+su --login - postgres --command "/usr/pgsql-${PGVERSION}/bin/psql -c \"CREATE USER madlib with CREATEROLE superuser PASSWORD 'password';\""
+su --login - postgres --command "/usr/pgsql-${PGVERSION}/bin/psql -c \"CREATE DATABASE maddb;\""
+su --login - postgres --command "/usr/pgsql-${PGVERSION}/bin/psql -c \"\du;\""
